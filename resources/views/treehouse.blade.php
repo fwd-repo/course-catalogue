d0be0c5d89a0881164d6783643d29f23427094fe
Ed Tansey <office.fwd@yahoo.com>
2021-08-24 11:26:39 +0100

    Connect app to database

--- resources/views/treehouse.blade.php
+++ resources/views/treehouse.blade.php
@@ -1,16 +1,11 @@
 <!doctype html>
 <html lang="en">
 @include('head')
-<!-- START body -->
 <body style="background-color: #edeff0 !important">
 @include('nav')
-
-    <!-- START library.blade.php -->
-    <main role="main" class="container">
-        <!-- @yield('body') -->
-    </main>
-    <!-- END library.blade.php -->
-
+<main role="main" class="container">
+    @yield('body')
+</main>
 @include('footer')
-</body> <!-- END Body -->
+</body>
 </html>
